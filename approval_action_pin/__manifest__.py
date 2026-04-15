41637e155471b55982c4a98ad27e1e21d6e5335f
pk-co <prasacode@email.com>
2026-04-16 00:45:40 +0530

    [UPD] banner

--- approval_action_pin/__manifest__.py
+++ approval_action_pin/__manifest__.py
@@ -16,7 +16,7 @@ Main features:
     "category": "Tools",
     "author": "Optin Solutions",
     "maintainer": "optindev",
-    #"website": "https://www.optinsolutions.com",
+    "website": "https://www.optinsolutions.com",
     "support": "optinassist@gmail.com",
     "license": "LGPL-3",
     "depends": [
