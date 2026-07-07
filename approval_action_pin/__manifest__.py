f9f0cc6db79905824825e7c1c8b4bb44b4479250
Prasankumara <118862409+pk-co@users.noreply.github.com>
2026-07-07 22:25:04 +0530

    Change price from 21.9 to 9.9

--- approval_action_pin/__manifest__.py
+++ approval_action_pin/__manifest__.py
@@ -17,7 +17,7 @@
     "author": "Optin Solutions",
     # "website": "https://www.optinsolutions.com",
     "support": "optinassist@gmail.com",
-    "price": 21.9,
+    "price": 9.9,
     "currency": "USD",
     "license": "LGPL-3",
     "depends": [
